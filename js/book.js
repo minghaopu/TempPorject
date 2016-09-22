d7706ca3b9ac66b5336698d6607fdf15b3a7c90d
roneilPMH <roneil_PMH@hotmail.com>
2016-09-21 21:56:00 -0400

    Create Project;

---
+++ js/book.js
@@ -0,0 +1,37 @@
+(function(window, undefined) {
+	/**
+	 * @constructor
+	 */
+	var Book = function() {
+
+	};
+
+	/**
+	 * @param {number} x
+	 * @returns {void}
+	 */
+	Book.prototype.create = function(author, bookname) {
+
+	};
+
+	/**
+	 * @returns {void}
+	 */
+	Book.prototype.delete = function(id) {
+
+	};
+
+	/**
+	 * @returns {number}
+	 */
+	Book.prototype.read = function(id) {
+
+	};
+
+	/**
+	 * @returns {boolean}
+	 */
+	Book.prototype.update = function(author, bookname, id) {
+
+	};
+})(window)
